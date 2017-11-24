9b1f40a1417714632c96a9b6ad09283572a742fb
Charlio Xu <chalioxumykj@gmail.com>
2017-11-23 21:31:05 -0800

    finish 4-create-a-movie-webiste

--- 4-Create-a-Movie-Website/3_make_classes/b_advanced/inheritance.py
+++ 4-Create-a-Movie-Website/3_make_classes/b_advanced/inheritance.py
@@ -19,5 +19,17 @@ class Parent():
 
 class Child(Parent):
     def __init__(self, last_name, eye_color, number_of_toys):
-
+        print("Child Constructor Called")
+        Parent.__init__(self, last_name, eye_color)
+        self.number_of_toys = number_of_toys
+        
     def show_info(self):
+        print("Last Name - " + self.last_name)
+        print("Eye Color - " + self.eye_color)
+        print("Number of Toys - " + str(self.number_of_toys))
+   
+daddy = Parent("Xu", "brown")
+daddy.show_info()
+
+dipper = Child("Xu", "black", 2)
+dipper.show_info()
