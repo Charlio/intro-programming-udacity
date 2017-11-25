24710dd9835a54e6fbb34035ad7b40a45a970753
Charlio Xu <chalioxumykj@gmail.com>
2017-11-25 04:35:38 -0800

    update movie website project

--- 4-Create-a-Movie-Website/3_make_classes/a_movies/entertainment_center.py
+++ 4-Create-a-Movie-Website/3_make_classes/a_movies/entertainment_center.py
@@ -12,40 +12,54 @@ import media
 import fresh_tomatoes
 
 
-kingdom_of_heaven = media.Movie("Kingdom of Heaven",
-                                "A knight to defend Jerusalem",
-                                "https://upload.wikimedia.org/wikipedia/en/9/9e/KoHposter.jpg",
-                                "https://www.youtube.com/watch?v=Kfq9U2tWWGo")
+kingdom_of_heaven = media.Movie(
+    "Kingdom of Heaven",
+    "A knight to defend Jerusalem",
+    "https://upload.wikimedia.org/wikipedia/en/9/9e/KoHposter.jpg",
+    "https://www.youtube.com/watch?v=Kfq9U2tWWGo")
 
-gladiator = media.Movie("Gladiator",
-                        "A man robbed of his name and dignity strives to win them back and gain the freedom of his people",
-                        "https://upload.wikimedia.org/wikipedia/en/8/8d/Gladiator_ver1.jpg",
-                        "https://www.youtube.com/watch?v=owK1qxDselE")  
- 
-alien = media.Movie("Alien", 
-                    "A close encounter of the third kind becomes a Jaws-style nightmare when an alien invades a spacecraft",
-                    "https://upload.wikimedia.org/wikipedia/en/f/fb/Aliens_poster.jpg",
-                    "https://www.youtube.com/watch?v=LjLamj-b0I8") 
-                    
-pacific_rim = media.Movie("Pacific Rim",
-                          "A ragtag band of humans banding together to fight monstrous creatures from the sea",
-                          "https://upload.wikimedia.org/wikipedia/en/f/f3/Pacific_Rim_FilmPoster.jpeg",
-                          "https://www.youtube.com/watch?v=5guMumPFBag")
-                          
-the_day_after_tomorrow = media.Movie("The Day after Tomorrow",
-                                     "A paleoclimatologist must reach his son trapped in a sudden international storm",
-                                     "https://upload.wikimedia.org/wikipedia/en/5/58/The_Day_After_Tomorrow_movie.jpg",
-                                     "https://www.youtube.com/watch?v=Ku_IseK3xTc")
-                     
-deep_impact = media.Movie("Deep Impact",
-                          "Humans attempt to destroy a comet expected to collide with the Earth and cause a mass extinction",
-                          "https://upload.wikimedia.org/wikipedia/en/9/93/Deep_Impact_poster.jpg",
-                          "https://www.youtube.com/watch?v=NTkfk4dCnu8")
-                     
-movies = [kingdom_of_heaven, gladiator, alien, pacific_rim, the_day_after_tomorrow, deep_impact]
+gladiator = media.Movie(
+    "Gladiator",
+    """A man robbed of his name and dignity strives to win them back and gain
+    the freedom of his people""",
+    "https://upload.wikimedia.org/wikipedia/en/8/8d/Gladiator_ver1.jpg",
+    "https://www.youtube.com/watch?v=owK1qxDselE")
 
-#print(media.Movie.VALID_RATINGS)
-#print(media.Movie.__name__)
-#print(media.Movie.__module__)
+alien = media.Movie(
+    "Alien",
+    """A close encounter of the third kind becomes a Jaws-style nightmare when
+    an alien invades a spacecraft""",
+    "https://upload.wikimedia.org/wikipedia/en/f/fb/Aliens_poster.jpg",
+    "https://www.youtube.com/watch?v=LjLamj-b0I8")
+
+pacific_rim = media.Movie(
+    "Pacific Rim",
+    """A ragtag band of humans banding together to fight monstrous creatures
+    from the sea""",
+    "https://upload.wikimedia.org/wikipedia/en/f/f3/Pacific_Rim_FilmPoster.jpeg",  # noqa
+    "https://www.youtube.com/watch?v=5guMumPFBag")
+
+the_day_after_tomorrow = media.Movie(
+    "The Day after Tomorrow",
+    """A paleoclimatologist must reach his son trapped in a sudden international
+    storm""",
+    "https://upload.wikimedia.org/wikipedia/en/5/58/The_Day_After_Tomorrow_movie.jpg",  # noqa
+    "https://www.youtube.com/watch?v=Ku_IseK3xTc")
+
+deep_impact = media.Movie(
+    "Deep Impact",
+    """Humans attempt to destroy a comet expected to collide with the Earth and
+    cause a mass extinction""",
+    "https://upload.wikimedia.org/wikipedia/en/9/93/Deep_Impact_poster.jpg",
+    "https://www.youtube.com/watch?v=NTkfk4dCnu8")
+
+movies = [
+    kingdom_of_heaven,
+    gladiator,
+    alien,
+    pacific_rim,
+    the_day_after_tomorrow,
+    deep_impact
+]
 
 fresh_tomatoes.open_movies_page(movies)
--- 4-Create-a-Movie-Website/3_make_classes/a_movies/media.py
+++ 4-Create-a-Movie-Website/3_make_classes/a_movies/media.py
@@ -8,18 +8,29 @@
 
 import webbrowser
 
-class Movie():
-    '''This class provides a way to store movie related information'''
-    VALID_RATINGS = ['G', 'PG', 'PG-13', 'R']
 
-    def __init__(self, title, storyline, poster_image_url, trailer_youtube_url):
-        # initialize instance of class Movie
+class Movie():
+    '''This class provides a way to store movie related information
+
+    This Movie class provides a uniform API to store information of
+    a movie
+
+    Attributes:
+        title: A string of the movie title.
+        storyline: A string of a short description of the movie.
+        poster_image_url: A string of the url of an moive poster.
+        trailer_youtube_url: A string of the url of a moive trailer
+                             from youtube.
+        '''
+
+    def __init__(self, title, storyline, poster_image_url,
+                 trailer_youtube_url):
+        '''initialize instance of class Movie'''
         self.title = title
         self.storyline = storyline
         self.poster_image_url = poster_image_url
         self.trailer_youtube_url = trailer_youtube_url
-        
+
     def show_trailer(self):
-        # open the youtube trailer in the default web browser
+        '''open the youtube trailer in the default web browser'''
         webbrowser.open(self.trailer_youtube_url)
-        
